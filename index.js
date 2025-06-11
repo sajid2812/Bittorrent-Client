a95fff3a2d1cfe58bbf147f58aa5dafc1e765675
Sk Sajid <ssk73573@gmail.com>
2025-06-11 22:31:01 +0530

    add tracker.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
+
 const fs = require("fs");
 const bencode = require("bencode");
+const tracker = require("./tracker");
 
 const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
 
-console.log(torrent.announce.toString("utf-8"));
+tracker.getPeers(torrent, (peers) => {
+  console.log("list of peers: ", peers);
+});
